b24ba77a98ba4a786f058f71efeb0e84fa24f282
Ionina34 <igoninadasha0315@mail.ru>
2022-05-07 16:04:23 +0500

    Add project files.

---
+++ Modul_3/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace Modul_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.LastNameTextBox = new System.Windows.Forms.TextBox();
+            this.MailTextBox = new System.Windows.Forms.TextBox();
+            this.PhoneTextBox = new System.Windows.Forms.TextBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.buttonImport = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Анкета";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(16, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(16, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Фамилия";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(16, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Е-mail";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(16, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Телефон";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(100, 40);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(196, 20);
+            this.NameTextBox.TabIndex = 5;
+            // 
+            // LastNameTextBox
+            // 
+            this.LastNameTextBox.Location = new System.Drawing.Point(100, 71);
+            this.LastNameTextBox.Name = "LastNameTextBox";
+            this.LastNameTextBox.Size = new System.Drawing.Size(196, 20);
+            this.LastNameTextBox.TabIndex = 6;
+            // 
+            // MailTextBox
+            // 
+            this.MailTextBox.Location = new System.Drawing.Point(100, 104);
+            this.MailTextBox.Name = "MailTextBox";
+            this.MailTextBox.Size = new System.Drawing.Size(196, 20);
+            this.MailTextBox.TabIndex = 7;
+            // 
+            // PhoneTextBox
+            // 
+            this.PhoneTextBox.Location = new System.Drawing.Point(100, 134);
+            this.PhoneTextBox.Name = "PhoneTextBox";
+            this.PhoneTextBox.Size = new System.Drawing.Size(196, 20);
+            this.PhoneTextBox.TabIndex = 8;
+            // 
+            // listBox
+            // 
+            this.listBox.ContextMenuStrip = this.contextMenuStrip;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.HorizontalScrollbar = true;
+            this.listBox.Location = new System.Drawing.Point(19, 224);
+            this.listBox.Name = "listBox";
+            this.listBox.ScrollAlwaysVisible = true;
+            this.listBox.Size = new System.Drawing.Size(277, 121);
+            this.listBox.TabIndex = 9;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.removeToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip1";
+            this.contextMenuStrip.Size = new System.Drawing.Size(118, 48);
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.removeToolStripMenuItem.Text = "Remove";
+            this.removeToolStripMenuItem.Click += new System.EventHandler(this.removeToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.editToolStripMenuItem.Text = "Edit";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click);
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExport.Location = new System.Drawing.Point(19, 359);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(119, 79);
+            this.buttonExport.TabIndex = 11;
+            this.buttonExport.Text = "Экспорт";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+            // 
+            // buttonImport
+            // 
+            this.buttonImport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonImport.Location = new System.Drawing.Point(177, 359);
+            this.buttonImport.Name = "buttonImport";
+            this.buttonImport.Size = new System.Drawing.Size(119, 79);
+            this.buttonImport.TabIndex = 12;
+            this.buttonImport.Text = "Импорт";
+            this.buttonImport.UseVisualStyleBackColor = true;
+            this.buttonImport.Click += new System.EventHandler(this.buttonImport_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(76, 175);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(168, 38);
+            this.buttonAdd.TabIndex = 13;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 450);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonImport);
+            this.Controls.Add(this.buttonExport);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.PhoneTextBox);
+            this.Controls.Add(this.MailTextBox);
+            this.Controls.Add(this.LastNameTextBox);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Анкета";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox LastNameTextBox;
+        private System.Windows.Forms.TextBox MailTextBox;
+        private System.Windows.Forms.TextBox PhoneTextBox;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.Button buttonImport;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.Button buttonAdd;
+    }
+}
+
---
+++ Modul_3/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modul_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.ForeColor = Color.Red;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NameTextBox.TextChanged += TextChenged;
+            LastNameTextBox.TextChanged += TextChenged;
+            MailTextBox.TextChanged += TextChenged;
+            PhoneTextBox.TextChanged += TextChenged;
+        }
+        private void TextChenged(object sender, EventArgs e)
+        {
+            TextBox text = sender as TextBox;
+            if (text.Text != "")
+                text.BackColor = Color.White;
+        }
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (NameTextBox.Text != "" && LastNameTextBox.Text != "" &&
+                MailTextBox.Text != "" && PhoneTextBox.Text != "")
+            {
+                listBox.Items.Add($"Пользователь: {NameTextBox.Text} {LastNameTextBox.Text}" +
+                    $" {MailTextBox.Text} {PhoneTextBox.Text}");
+
+                NameTextBox.Text = "";
+                LastNameTextBox.Text = "";
+                MailTextBox.Text = "";
+                PhoneTextBox.Text = "";
+
+                LastNameTextBox.BackColor = Color.White;
+                MailTextBox.BackColor = Color.White;
+                PhoneTextBox.BackColor = Color.White;
+            }
+            else
+            {
+                if (NameTextBox.Text == "")
+                    NameTextBox.BackColor = Color.FromArgb(255, 98, 90);
+                if (LastNameTextBox.Text == "")
+                    LastNameTextBox.BackColor = Color.FromArgb(255, 98, 90);
+                if (MailTextBox.Text == "")
+                    MailTextBox.BackColor = Color.FromArgb(255, 98, 90);
+                if (PhoneTextBox.Text == "")
+                    PhoneTextBox.BackColor = Color.FromArgb(255, 98, 90);
+
+                MessageBox.Show("Не все поля зополнены!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+       
+        private void removeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (removeToolStripMenuItem.Checked == false)
+            {
+                removeToolStripMenuItem.Checked = true;
+                editToolStripMenuItem.Checked = false;
+                if (listBox.Items.Count != 0)
+                {
+                    if (listBox.SelectedItems != null)
+                    {
+                        for (int i = 0; i < listBox.SelectedItems.Count; i++)
+                            listBox.Items.Remove(listBox.SelectedItems[i]);
+                        removeToolStripMenuItem.Checked = false;
+                    }
+                }
+            }
+            else
+                removeToolStripMenuItem.Checked = false;
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string user;
+            if (editToolStripMenuItem.Checked == false)
+            {
+                editToolStripMenuItem.Checked = true;
+                removeToolStripMenuItem.Checked = false;
+                if (listBox.Items.Count != 0)
+                {
+                    if (listBox.SelectedItems!= null)
+                    {
+                        for (int i = 0; i < listBox.SelectedItems.Count; i++)
+                        {
+                            user = listBox.SelectedItems[i].ToString();
+                            string[] strok = user.Split(' ');
+                            NameTextBox.Text = strok[1];
+                            LastNameTextBox.Text = strok[2];
+                            MailTextBox.Text = strok[3];
+                            PhoneTextBox.Text = strok[4];
+                            listBox.Items.Remove(listBox.SelectedItems[i]);
+                            editToolStripMenuItem.Checked = false;
+                        }
+                    }
+                }
+            }
+            else
+                editToolStripMenuItem.Checked = false;
+        }
+
+        private void buttonExport_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Text Files (*.txt;*.xml)|*.txt;*.xml||";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                TextWriter writer = new StreamWriter(save.FileName);
+                foreach (var item in listBox.Items)
+                    writer.WriteLine(item.ToString());
+                writer.Close();
+            }
+        }
+
+        private void buttonImport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text Files (*.txt;*.xml)|*.txt;*.xml|All files (*.*)|*.*||";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                listBox.Items.AddRange(File.ReadAllLines(ofd.FileName, Encoding.UTF8));
+            }
+        }
+    }
+}
